e5877283e7b01afda7488ad3b72f4f175cee52ad
Alejandro Seguí <alesegdia@gmail.com>
2016-09-27 21:37:03 +0200

    updating test and xstf

--- src/test/tst-engine.cpp
+++ src/test/tst-engine.cpp
@@ -1,45 +1,32 @@
-
 #include <secs/secs.h>
+#include <xstf.h>
 
-class Component1
+struct Component1 { uint8_t a; };
+struct Component2 { uint16_t a; };
+struct Component3 { uint32_t a; };
+
+static size_t SystemA_Counter = 0;
+class SystemA : public secs::TypedEntitySystem<Component1, Component2>
 {
 public:
-	int a, b, c;
-};
-
-class Component2
-{
-public:
-	int x, y, z;
-};
-
-class Component3
-{
-public:
-	int r, s, t;
-};
-
-class MySystem : public secs::EntitySystem
-{
-public:
-	MySystem( secs::EntityProcessor::Ptr processor )
-		: m_processor(processor)
+	void process(float delta, const secs::Entity &e, Component1& c1, Component2& c2 ) override
 	{
-		setNeededComponents<Component1, Component2>();
+		c1.a = 0xDE;
+		c2.a = 0xDEAD;
+		SystemA_Counter++;
 	}
+};
 
-	// EntitySystem interface
-	void onAdded(const secs::Entity &e) {}
-	void onRemoved(const secs::Entity &e) {}
-
-	void process(const secs::Entity &e)
+static size_t SystemB_Counter = 0;
+class SystemB : public secs::TypedEntitySystem<Component1, Component3>
+{
+public:
+	void process(float delta, const secs::Entity &e, Component1& c1, Component3& c3 ) override
 	{
-
+		c1.a = 0xDE;
+		c3.a = 0xDEADBEEF;
+		SystemB_Counter++;
 	}
-
-private:
-	secs::EntityProcessor::Ptr m_processor;
-
 };
 
 int main( int argc, char** argv )
@@ -47,11 +34,33 @@ int main( int argc, char** argv )
 	secs::Engine world;
 	secs::EntityProcessor& processor = world.processor();
 
-	secs::Entity e1 = processor.addEntity();
 
-	Component1& e1c1 = processor.addComponent<Component1>( e1 );
-	Component2& e1c2 = processor.addComponent<Component2>( e1 );
-	Component3& e1c3 = processor.addComponent<Component3>( e1 );
+	XSTF_TESTING("sECS");
+	XSTF_ENABLE_VERBOSE_CHECKS;
 
+	{
+		XSTF_TEST("System edits entity component");
+
+		secs::Entity e1 = processor.addEntity();
+
+		Component1& e1c1 = processor.addComponent<Component1>( e1 );
+		Component2& e1c2 = processor.addComponent<Component2>( e1 );
+
+		SystemA sa;
+		SystemB sb;
+
+		world.pushSystem(&sa);
+		world.pushSystem(&sb);
+
+		world.step(0);
+
+		XSTF_ASSERT(e1c1.a == 0xDE, "typed system component modification");
+	}
+
+	{
+		XSTF_TEST("System removes component");
+	}
+
+	return XSTF_TESTING_RESULT;
 
 }
